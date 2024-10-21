f88b2e3c3d4aaadd594a270a746fb903410b43a4
Hiba Bentaleb <hiba.bentaleb2019@gmail.com>
2024-10-21 22:07:38 +0100

    code game 3

---
+++ inventory/Food.java
@@ -0,0 +1,13 @@
+package inventory;
+
+public class Food extends Product{
+String expirationDate;
+public Food(String name, double price, int quantity, String expirationDate) {
+	super(name, price, quantity);
+	this.expirationDate=expirationDate;
+}
+public void displayInfo() {
+	super.displayInfo();
+	System.out.println(this.expirationDate);
+}
+}
